9deb76e354e88b0ea0981fe883b7a1b889347ef3
theo <lesgooo1993@proton.me>
2023-12-22 23:08:45 +0530

    added ps1; implemented upper command

--- main.v
+++ main.v
@@ -1,12 +1,23 @@
 import os
 
-fn prompt() {
-	command := os.input("$ ").split(' ')
-	println(command)
+fn upper(cmd []string) {
+	// println(cmd[1..].map(it.to_upper()).join(' '))
+	println(cmd[1..].join(' ').to_upper())
+}
+
+fn prompt(prompt_str string) {
+	command := os.input(prompt_str).split(' ')
+
+	if command[0] == "upper" {
+		upper(command)
+	} else {
+		println(command)
+	}
 }
 
 fn main() {
+	prompt_str := "$ "
 	for {
-		prompt()
+		prompt(prompt_str)
 	}
 }
